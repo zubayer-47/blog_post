f5f36d8da66dac622d213662610e6fbfed65bfa2
ZubayerJS <zubayerjs.dev@gmail.com>
2023-06-18 21:54:06 +0600

    changess

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -57,7 +57,7 @@ const Navbar = () => {
             </button>
             <Link href={"/profile"}>
               <Image
-                src={session?.user.image}
+                src={session?.user?.image}
                 alt="logo"
                 width={37}
                 height={37}
@@ -114,7 +114,7 @@ const Navbar = () => {
                   <button
                     type="button"
                     key={provider.name}
-                    onClick={() => signIn(provider.id)}
+                    onClick={() => signIn('google')}
                     className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
                   >
                     <svg
